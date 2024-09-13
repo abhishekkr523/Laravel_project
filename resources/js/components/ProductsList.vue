b696bd7f608f4f9506a533f680e336e680f6e26f
abhishekkr523 <7033abhishek@gmail.com>
2024-09-13 08:05:10 +0530

    implement product update functionality

--- resources/js/components/ProductsList.vue
+++ resources/js/components/ProductsList.vue
@@ -54,6 +54,26 @@
                             <option value="DESC">DESC</option>
                         </select>
                      </div>
+                     <div>
+                      <label for="condition" class="select-label">Filter by condition: </label>
+                      <select
+  id="conditions"
+  v-model="selectedCondition"
+  @change="filterByCondition"
+  class="select-filter"
+>
+  <option value="">All</option>
+  <option
+    v-for="condition in conditions"
+    :key="condition.id"
+    :value="condition.id"
+  >
+    {{ condition.product_condition }}
+  </option>
+</select>
+
+                    </div>
+                     
                 </form>
             </div>
         </div>
@@ -62,70 +82,117 @@
 
 <script>
 
-    export default{
+   export default {
+  name: 'ProductsList',
 
-        name: 'ProductsList',
+  data() {
+    return {
+      title: 'Products List',
+      products: [],
+      categories: [],
+      conditions: [],  
+      selectedCategory: '',
+      selectedOrder: '',
+      selectedCondition: '',  // Add this property
+    };
+  },
 
-        data() {
-            return {
-                title: 'Products List',
-                products: [],
-                categories: [],
-                selectedCategory:'',
-                selectedOrder:'',
-            }
-        },
+  methods: {
+    getProducts: function () {
+      axios.get(`${window.location.protocol}//${window.location.host}/api/products`)
+        .then(response => {
+          this.products = response.data.products;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
 
-        methods:{
+    getCategories: function () {
+      axios.get(`${window.location.protocol}//${window.location.host}/api/categories`)
+        .then(response => {
+          this.categories = response.data.categories;
+          console.log("iii", this.categories);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
 
-            getProducts: function(){
-                axios.get(`${window.location.protocol}//${window.location.host}/api/products`)
-                     .then(response => {
-                        console.log("uu",response)
-                        this.products = response.data.products;})
-                     .catch(error => { console.log(error);});
-            },
+   getCondition: function () {
+  axios.get(`${window.location.protocol}//${window.location.host}/api/product-conditions`)
+    .then(response => {
+      this.conditions = response.data;  // Ensure this.data is the array you expect
+      console.log("iiiw", this.conditions);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+},
 
-            getCategories: function(){
-                axios.get(`${window.location.protocol}//${window.location.host}/api/categories`)
-                     .then(response => { this.categories = response.data.categories;})
-                     .catch(error => { console.log(error);});
-            },
 
-            filterByCategory: function(){
-                axios.get(`${window.location.protocol}//${window.location.host}/api/products/${this.selectedCategory}`)
-                    .then(response => { this.products = response.data.products;})
-                    .catch(error => { console.log(error);});
-            },
 
-            sortByOrder: function(){
-                if (this.selectedOrder === "ASC") {
-                    this.products = this.products.sort((a, b) => a.price - b.price);
-                }
-                if (this.selectedOrder === "DESC") {
-                    this.products = this.products.sort((a, b) => b.price - a.price);
-                }
-            },
 
-             goToUpdateProduct(id) {
-            this.$router.push({ path: `/update-product/${id}` });  // Navigate to UpdateProduct component
-        },
+    filterByCategory: function () {
+      axios.get(`${window.location.protocol}//${window.location.host}/api/products/${this.selectedCategory}`)
+        .then(response => {
+          this.products = response.data.products;
+          console.log("filterByCategory",this.products)
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
 
-         deleteProduct(id) {
-            axios.delete(`${window.location.protocol}//${window.location.host}/api/products/${id}`)
-                 .then(response => {
-                    // Remove the deleted product from the local list
-                    this.products = this.products.filter(product => product.id !== id);
-                 })
-                 .catch(error => { console.log(error); });
-        }
-        },
+    sortByOrder: function () {
+      if (this.selectedOrder === "ASC") {
+        this.products = this.products.sort((a, b) => a.price - b.price);
+      }
+      if (this.selectedOrder === "DESC") {
+        this.products = this.products.sort((a, b) => b.price - a.price);
+      }
+    },
 
-        created() {
-            this.getProducts();
-            this.getCategories();
-        },
+    filterByCondition: function () {
+      if (this.selectedCondition) {
+        this.products = this.products.filter(product => product.condition === this.selectedCondition);
+      } else {
+        this.getProducts();  // Reload all products if no condition is selected
+      }
+
+    // axios.get(`${window.location.protocol}//${window.location.host}/api/products/${this.selectedCategory}`)
+    //     .then(response => {
+    //       this.selectedCondition = response.data.products;
+    //        console.log(this.selectedCondition);
+    //     })
+    //     .catch(error => {
+    //       console.log(error);
+    //     });
+    },
+
+    goToUpdateProduct(id) {
+      this.$router.push({ path: `/update-product/${id}` });  // Navigate to UpdateProduct component
+    },
+
+    deleteProduct(id) {
+      axios.delete(`${window.location.protocol}//${window.location.host}/api/products/${id}`)
+        .then(response => {
+          // Remove the deleted product from the local list
+          this.products = this.products.filter(product => product.id !== id);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
+  },
+
+  created() {
+    this.getProducts();
+    this.getCategories();
+     this.getCondition();
+  }
+};
+
 
 </script>
 
